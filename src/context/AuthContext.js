f71c9c0845bd6317fa597a9ebcb12c3bf3e03d10
Prachod Kakatur <prachodkakatur@Prachods-MacBook-Air.local>
2026-03-03 22:39:25 -0600

    Fix popup z-index on mobile and add page refresh on logout

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -24,6 +24,7 @@ export const AuthProvider = ({ children }) => {
   const logout = () => {
     setCurrentUser(null);
     localStorage.removeItem('nbaScorigamiUser');
+    window.location.reload();
   };
 
   return (
